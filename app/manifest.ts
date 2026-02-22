0d6f9bb7c4fcf5ceb27d4b4c97d94908a74e71eb
mariuscdhub <marius.coudurier.1423@gmail.com>
2026-02-22 17:03:50 +0100

    fix: explicit pwa installability requirements for Android Chrome

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,17 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/icon-192',
+                src: '/icon-192.png',
                 sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: '/icon-512',
+                src: '/icon-512.png',
                 sizes: '512x512',
                 type: 'image/png',
             },
             {
-                src: '/apple-icon?size=180x180',
+                src: '/apple-touch-icon.png',
                 sizes: '180x180',
                 type: 'image/png',
             },
